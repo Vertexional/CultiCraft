a2eb1b61ad28db22c3139ed47844f0e538a436a4
Superboss12309 <ggghhh1122334455667788@gmail.com>
2023-10-28 15:54:48 +1300

    Added Pill

--- src/main/java/net/vertexional/culticraft/CultiCraft.java
+++ src/main/java/net/vertexional/culticraft/CultiCraft.java
@@ -10,6 +10,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.vertexional.culticraft.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -19,6 +20,8 @@ public class CultiCraft {
     private static final Logger LOGGER = LogUtils.getLogger();
     public CultiCraft() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
---
+++ src/main/java/net/vertexional/culticraft/item/ModItems.java
@@ -0,0 +1,24 @@
+package net.vertexional.culticraft.item;
+
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.vertexional.culticraft.CultiCraft;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, CultiCraft.MOD_ID);
+
+    public static final RegistryObject<Item> BASIC_PILL = ITEMS.register("basic_pill",
+    () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(16).food(new FoodProperties.Builder().nutrition(0).alwaysEat().fast().effect(() -> new MobEffectInstance(MobEffects.HUNGER, 100, 5), 1).build())));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
